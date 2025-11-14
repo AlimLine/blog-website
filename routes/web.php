ba3ac7a0191ead2518f8075b8b22cc628f9dfb69
Alimline <Alimzhanb@freedombank.kz>
2025-11-14 17:54:40 +0500

    remove-kk

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/kk', function () {
+Route::get('/', function () {
   return view('page');
 });
 
-Route::get('/kk/admin', function () {
+Route::get('/admin', function () {
   return view('admin');
 });
