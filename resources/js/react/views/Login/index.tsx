05b5c8cdb005bd43852449bf857853feb364fa44
Alimline <Alimzhanb@freedombank.kz>
2025-12-05 11:07:00 +0500

    .

--- resources/js/react/views/Login/index.tsx
+++ resources/js/react/views/Login/index.tsx
@@ -1,24 +1,70 @@
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 import {createReactRoot} from '@/helpers/createReactRoot.jsx'
 import styles from "./page.module.scss"
-import Header from "@/views/MainPage/_components/Header/Header";
 import iBackGround from "@/assets/MainPage/background.png";
-import Blogs from "@/views/MainPage/_components/Blogs/Blogs";
+import {Button} from "@mui/material";
 
-const MainPage = () => {
+interface LoginDataProps {
+  name: string
+  password: string
+}
+
+const initialLoginData: LoginDataProps = {
+  name: '',
+  password: ''
+}
+
+const Login = () => {
+  const [loginData, setLoginData] = useState<LoginDataProps>(initialLoginData)
+
+  const onSubmit = () => {
+
+  }
+
+  const handleOnChangeEvent = (e: ChangeEvent<HTMLInputElement>) => {
+    const {value, name} = e.currentTarget
+
+    setLoginData({
+      ...loginData,
+      [name]: value
+    })
+  }
+
   return (
     <div className={styles.page}>
       <img src={iBackGround} alt="" className={styles.bg} />
 
       <div className={styles.container}>
-        <Header />
+        <form action={onSubmit} className={styles.form}>
+          <h3>Вход</h3>
 
-        <Blogs />
+          <div className={styles.inputs}>
+            <input
+              name='name'
+              value={loginData.name}
+              onChange={handleOnChangeEvent}
+              className={styles.input}
+              placeholder='Логин'
+            />
+
+            <input
+              name='password'
+              value={loginData.password}
+              onChange={handleOnChangeEvent}
+              className={styles.input}
+              placeholder='Пароль'
+            />
+          </div>
+
+          <Button type='submit' className={styles.submitButton} variant='outlined' color='secondary'>
+            Войти
+          </Button>
+        </form>
       </div>
     </div>
   )
 }
 
-export default MainPage
+export default Login
 
-createReactRoot('loginReactRoot', MainPage)
+createReactRoot('loginReactRoot', Login)
